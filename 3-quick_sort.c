960dbef1b7ea1c8ba909c06c0b9de598c1a67823
meddizzle316 <meddizzle316@gmail.com>
2023-12-20 15:17:50 -0600

    added print array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -21,11 +21,12 @@ void swap(int *x, int *y)
  * @array: array to be sorted
  * @low: low index of array
  * @high: high index of array
+ * @size: size of array for printing
  *
  * Return: value around which partition will be made
  */
 
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, size_t size)
 {
 	int pivot_value, i, j;
 
@@ -37,9 +38,11 @@ int partition(int *array, int low, int high)
 		{
 			swap(&array[i], &array[j]);
 			i++;
+			print_array(array, size);
 		}
 	}
 	swap(&array[i], &array[high]);
+	print_array(array, size);
 	return (i);
 }
 
@@ -48,18 +51,19 @@ int partition(int *array, int low, int high)
  * @array: given array to be sorted
  * @low: low index of array, bottom end of partition
  * @high: high index of array, top end of partition
+ * @size: size for print function
  *
  * Return: always void
  */
-void quicksort_recursion(int *array, int low, int high)
+void quicksort_recursion(int *array, int low, int high, size_t size)
 {
 	int pivot_index;
 
 	if (low < high)
 	{
-		pivot_index = partition(array, low, high);
-		quicksort_recursion(array, low, pivot_index - 1);
-		quicksort_recursion(array, pivot_index + 1, high);
+		pivot_index = partition(array, low, high, size);
+		quicksort_recursion(array, low, pivot_index - 1, size);
+		quicksort_recursion(array, pivot_index + 1, high, size);
 	}
 }
 
@@ -73,5 +77,5 @@ void quicksort_recursion(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	quicksort_recursion(array, 0, size - 1);
+	quicksort_recursion(array, 0, size - 1, size);
 }
