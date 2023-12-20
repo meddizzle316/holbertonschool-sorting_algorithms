91ec9dd3fc0b31b264acddff2caf5bfca6a094a1
meddizzle316 <meddizzle316@gmail.com>
2023-12-20 11:32:41 -0600

    first go at 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,58 +1,45 @@
 #include "sort.h"
 /**
- * sortedInsert - creates a new doubly linked list and adds nodes in order
- * @head: head of list
- * @newNode: newnode to add
- *
- * Return: always void
- */
-
-void sortedInsert(listint_t** head, listint_t* newNode)
-{
-	listint_t* current;
-
-	if (*head == NULL)
-		*head = newNode;
-	else if ((*head)->n >= newNode->n)
-	{
-		newNode->next = *head;
-		newNode->next->prev = newNode;
-		*head = newNode;
-	}
-	else
-	{
-		current = *head;
-		while (current->next != NULL && current->next->n < newNode->n)
-		{
-			current = current->next;
-		}
-		newNode->next = current->next;
-		if (current->next != NULL)
-		{
-			newNode->next->prev = newNode;
-		}
-		current->next = newNode;
-		newNode->prev = current;
-	}
-}
-/**
- * insertion_sort_list - uses insert sort on a list
- * @list: doubly linked list to be sorted
+ * insertion_sort_list - performs insertion sort on doubly linked list
+ * @list: header to list
  *
  * Return: always void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t* sorted = NULL;
-	listint_t* next = NULL;
-
-	listint_t *current = *list;
-	while (current != NULL)
+	int i, temp, has_swapped, j;
+	listint_t *current, *back, *next, *prev;
+	
+	if (*list == NULL)
+		return;
+	current = (*list)->next;
+	for (i = 0; current != NULL; i++, current = current->next)
 	{
-		next = current->next;
-		current->prev = current->next = NULL;
-		sortedInsert(&sorted, current);
-		current = next;
+		j = i - 1;
+		temp = current->n;
+		back = current->prev;
+		has_swapped = 0;
+		while (j >= 0 && back != NULL && back->n > temp)
+		{
+			if (back->next->next != NULL)
+				back->next->next->prev = back;
+			next = back->next->next;
+			back->next->prev = back->prev;
+			back->next->next = back;
+			prev = back->prev;
+			if (back->prev != NULL)
+				back->prev->next = back->next;
+			back->prev = back->next;
+			back->next = next;
+			back = prev;
+			has_swapped = 1;
+			j--;
+		}
+		if (j < 0)
+		{
+			*list = current;
+		}
+		if (has_swapped == 1)
+			print_list(*list);
 	}
-	*list = sorted;
 }
